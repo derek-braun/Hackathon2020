5916f43ca6c6e321523ec805e1ef9f83ab05cbcc
derek-braun <derek.braun.99@gmail.com>
2020-02-15 14:26:50 -0700

    Add default page

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var session = require('express-session');
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//use sessions for tracking logins
+app.use(session({
+    secret: 'Jordon Bad',
+    resave: true,
+    saveUninitialized: false
+}));
+
+//Redirect main page to login
+app.get("/", function (req, res){
+    //redirect to login
+    res.redirect("/default");
+});
+
+app.get("/default", function (req, res){
+    res.render("default");
+});
+
+//Create server, wait for requests
+app.listen(8080, function(){
+    console.log("Server Running on Port 8080");
+});
