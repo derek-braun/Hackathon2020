08cfa44f0066ba8bf1c2b9cc7e4ef969f4a62120
derek-braun <derek.braun.99@gmail.com>
2020-02-16 05:57:52 -0700

    Text to speech

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
+;
 var dotenv = require('dotenv');
 var express = require('express');
 var mongoose = require('mongoose');
@@ -12,7 +13,69 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/img', express.static(__dirname + '/Images'));
+app.use('/root', express.static(__dirname + '/'));
 
+// Requires request and request-promise for HTTP requests
+// e.g. npm install request request-promise
+const rp = require('request-promise');
+// Requires fs to write synthesized speech to a file
+const fs = require('fs');
+// Requires readline-sync to read command line inputs
+const readline = require('readline-sync');
+// Requires xmlbuilder to build the SSML body
+const xmlbuilder = require('xmlbuilder');
+
+// Gets an access token.
+function getAccessToken(subscriptionKey) {
+    let options = {
+        method: 'POST',
+        uri: 'https://canadacentral.api.cognitive.microsoft.com/sts/v1.0/issueToken',
+        headers: {
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    }
+    return rp(options);
+}
+
+// Make sure to update User-Agent with the name of your resource.
+// You can also change the voice and output formats. See:
+// https://docs.microsoft.com/azure/cognitive-services/speech-service/language-support#text-to-speech
+function textToSpeech(accessToken, text) {
+    // Create the SSML request.
+    let xml_body = xmlbuilder.create('speak')
+        .att('version', '1.0')
+        .att('xml:lang', 'en-us')
+        .ele('voice')
+        .att('xml:lang', 'en-us')
+        .att('name', 'Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)')
+        .txt(text)
+        .end();
+    // Convert the XML into a string to send in the TTS request.
+    let body = xml_body.toString();
+
+    let options = {
+        method: 'POST',
+        baseUrl: 'https://canadacentral.tts.speech.microsoft.com/',
+        url: 'cognitiveservices/v1',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'cache-control': 'no-cache',
+            'User-Agent': 'Hackathon2020',
+            'X-Microsoft-OutputFormat': 'riff-16khz-16bit-mono-pcm',
+            'Content-Type': 'application/ssml+xml'
+        },
+        body: body
+    }
+
+    let request = rp(options)
+        .on('response', (response) => {
+            if (response.statusCode === 200) {
+                request.pipe(fs.createWriteStream('TTSOutput.wav'));
+                console.log('\nYour file is ready.\n')
+            }
+        });
+    return request;
+}
 
 // Connect to MongoDB using Mongoose
 var mongoUrl = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DB}`
@@ -79,28 +142,52 @@ db.once('open', () => {
         req.body.username
     });
 
-app.get("/signup", function(req, res){
-    res.render("signup");
-});
+    app.get("/signup", function(req, res){
+        res.render("signup");
+    });
 
-app.post("/signup", function(req, res){
-});
+    app.post("/signup", function(req, res){
+    });
 
-app.get("/patientDirectory", function(req, res){
-    res.render("patientDirectory");
-});
+    app.get("/patientDirectory", function(req, res){
+        res.render("patientDirectory");
+    });
 
-// Prevent internal page access without login
-app.use(function(req, res, next) {
-    if(req.session && req.session.userId) {
-        next();
-    } else {
-        res.redirect("/");
-    }
-});
+    app.get("/reband", function(req, res){
+        res.render("reband");
+    });
 
-    app.get("/tasks", function (req, res){
+    // Prevent internal page access without login
+    app.use(function(req, res, next) {
+        if(req.session && req.session.userId) {
+            next();
+        } else {
+            res.redirect("/");
+        }
+    });
+
+    app.get("/tasks", async function (req, res){
         res.render("tasks");
+    });
+
+    app.post("/tasks", async function(req, res){
+        console.log(req.body.recording);
+        const subscriptionKey = "0ad85ed05628469eb188c20579535bfe";
+        if (!subscriptionKey) {
+            throw new Error('Environment variable for your subscription key is not set.')
+        };
+        // Prompts the user to input text.
+        const text = req.body.recording;
+
+        try {
+            const accessToken = await getAccessToken(subscriptionKey);
+            await textToSpeech(accessToken, text);
+        } catch (err) {
+            console.log(`Something went wrong: ${err}`);
+        }
+        setTimeout(function() {
+            return res.redirect("/tasks");
+        }, 50);
     });
 
     app.get("/addTask", function (req, res){
@@ -111,4 +198,5 @@ app.use(function(req, res, next) {
     app.listen(8080, function(){
         console.log("Server Running on Port 8080");
     });
+
 });
